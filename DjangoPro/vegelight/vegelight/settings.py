c5ad9231780cacda11bfce92e6ba352dcbcc6439
Sushravya40 <sushravya1723@gmail.com>
2026-05-11 23:20:16 +0530

    Fix template case sensitivity for Linux deployment

--- DjangoPro/vegelight/vegelight/settings.py
+++ DjangoPro/vegelight/vegelight/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'vegelight.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'delivery' / 'Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
