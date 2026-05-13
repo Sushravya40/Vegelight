608cbed0152773d7684884a4a27ccd3556c5db84
Sushravya40 <sushravya1723@gmail.com>
2026-05-13 12:18:51 +0530

    Add improved styled Planr React application

---
+++ Planr/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState,useEffect } from "react";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+import ProgressTracker from "./components/ProgressTracker";
+import './Style.css'
+
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(()=>{
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+  });
+
+  const addTask = (task)=>{
+    setTasks([...tasks, task])
+  }
+
+  const updateTask = (updatedTask, index) => {
+    const newtask = [...tasks];
+    newtask[index] = updatedTask;
+    setTasks(newtask);
+  }
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i != index ))
+  }
+
+  const clearTasks = () => {
+    setTasks([]);
+  }
+  return (
+    <div className="App">
+      <header>
+        <h1>TaskMan</h1>
+        <p><i>Your friendly Task Manager</i></p>
+      </header>
+
+      <TaskForm addTask = {addTask}/>
+      <TaskList tasks = {tasks}
+       updateTask = {updateTask} 
+       deleteTask = {deleteTask}/>
+      <ProgressTracker tasks = {tasks}/>
+
+      {tasks.length>0 && (<button className='clear-btn'
+      onClick={clearTasks}>clear All Tasks</button>)}
+      
+    </div>
+  )
+}
